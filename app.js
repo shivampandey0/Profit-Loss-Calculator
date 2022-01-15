99e512e3ae90b07cad7ac28cd69053eac8de69bd
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-01-15 11:06:12 +0530

    refactor: code upgraded to es6

--- app.js
+++ app.js
@@ -6,41 +6,31 @@ const buyPriceLabel = document.querySelector(".price-on-date");
 const submit = document.querySelector(".check");
 const process = document.querySelector(".timeout")
 
-
-submit.addEventListener("click", clickHandler);
-
-var currentPriceURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
-var historicalPriceURL = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=";
+const currentPriceURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+const historicalPriceURL = "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=";
 
 
-
-
-function clickHandler() {
-
+const clickHandler = () => {
     if (purchaseDate.value && quantity.value) {
         showProcess();
-
-       
-        fetchPrices();   
-    }else{
+        fetchPrices();
+    } else {
         updateResult("Invalid Date or Quantity", "grey");
-    
-    }    
+    }
 }
 
-async function fetchPrices()  {
-    var boughtAt = 0;
-    var currentPrice = 0;
-
+async function fetchPrices() {
+    let boughtAt = 0;
+    let currentPrice = 0;
 
     await fetch(currentPriceURL)
-    .then(response => response.json())
-    .then(data => currentPrice = data["bitcoin"]["usd"]);
-    
-    await fetch(historicalPriceURL + formatDate())
-    .then(response => response.json())
-    .then(data => boughtAt = Math.round(data["market_data"]["current_price"]["usd"]));
-    
+        .then(response => response.json())
+        .then(data => currentPrice = data["bitcoin"]["usd"]);
+
+    await fetch(historicalPriceURL + formatDate(purchaseDate.value))
+        .then(response => response.json())
+        .then(data => boughtAt = Math.round(data["market_data"]["current_price"]["usd"]));
+
     setTimeout(() => {
         hideProcess();
     }, 1000);
@@ -51,38 +41,39 @@ async function fetchPrices()  {
     calculateReturns(boughtAt, currentPrice, quantity.value)
 }
 
-function calculateReturns(buy, current, quantity) {
+const calculateReturns = (buy, current, quantity) => {
     if (buy > current) {
-        var loss = (buy - current) * quantity;
-        var lossPercentage = Math.round((loss / buy) * 100);
+        const loss = (buy - current) * quantity;
+        const lossPercentage = Math.round((loss / buy) * 100);
 
-        updateResult(`Oops! You're down by ${lossPercentage}% with total loss of $${loss}`,"red");
-    } else{
-        var profit = (current - buy) * quantity;
-        var profitPercentage = Math.round((profit / buy) * 100);
+        updateResult(`Oops! You're down by ${lossPercentage}% with total loss of $${loss}`, "red");
+    } else {
+        const profit = (current - buy) * quantity;
+        const profitPercentage = Math.round((profit / buy) * 100);
 
         updateResult(`Kudos! You're in profit by ${profitPercentage}% with total profit of $${profit}`, "green")
-        
+
     }
 }
 
-function formatDate() {  
-    return purchaseDate.value.split("-").reverse().join("-"); 
-}
+const formatDate = date => date.split("-").reverse().join("-");
 
-function updateResult(message,message_color) {
+
+const updateResult = (message, message_color) => {
     result.innerText = message;
     result.style.color = message_color;
 }
 
-function showProcess() {
+const showProcess = () => {
     process.style.display = "block";
     result.style.display = "none";
 
 }
 
-function hideProcess() {
+const hideProcess = () => {
     process.style.display = "none";
     result.style.display = "block";
-    
 }
+
+
+submit.addEventListener("click", clickHandler);
